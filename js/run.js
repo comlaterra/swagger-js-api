6b0f22479a9b1e5d241294fbe70e0cf6ec1777e6
Edmon <comlaterra@gmail.com>
2015-12-01 16:42:13 +0100

    Add button to rerun

--- js/run.js
+++ js/run.js
@@ -1,1 +1,14 @@
-var API = new swaggerAPI();
+window.onload = function(){
+  function run(){
+    var swaggerOptions = {
+      "serverInfo":{
+        "basePath": 'http://alpha.calendar42.com/api'
+      },
+      "operations":{
+
+      }
+    }
+    var API = new swaggerAPI(swaggerOptions);
+  }
+  document.getElementById("runner").addEventListener("click", run);
+}
