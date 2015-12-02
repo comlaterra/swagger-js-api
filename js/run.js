7017648ac8d0f29b9869cf550713b7e0d5d7b1f0
Edmon <comlaterra@gmail.com>
2015-12-02 09:41:07 +0100

    Add the option to set a mapping between userConfig and enpoint operations name

--- js/run.js
+++ js/run.js
@@ -5,10 +5,19 @@ var API;
 function run(){
   var swaggerOptions = {
     "serverInfo":{
-      "basePath": 'http://beta.calendar42.com/api'
+      "basePath": 'http://demo.calendar42.com/api'
     },
     "operations":{
-
+      "Calendar_Api_get_calendars":"getCalendars",
+      "Event_Subscription_Api_post_event_subscription" : "postEventSubscription",
+      "Event_Subscription_Api_get_event_subscriptions" : "getEventSubscription",
+      "Event_Api_post_event" : "postEvent",
+      "Event_Api_get_events" : "getEvents",
+      "Location_Api_get_locations" : "getLocations",
+      "Position_Api_post_position" : "postPosition",
+      "Search_Event_Api_search_events" : "searchEvents",
+      "Service_Api_get_service_by_id" : "getServiceById",
+      "User_Attendance_Api_get_attendances" : "getAttendances"
     }
   };
   API = new swaggerAPI(swaggerOptions);
